bbddfc5537b3257452618c2918f1f0a3d714cfc1
xmarkgs <mraner123@gmail.com>
2019-12-25 13:00:18 +0300

    Bug Fix Commit
    Multiple arithmetic actions fixed.
    ZoomIn on mobile devices fixed.

--- minimalistcalc/js/main.js
+++ minimalistcalc/js/main.js
@@ -14,9 +14,40 @@ function equal(){
 		textview.value = Math.round(eval(valuenumber) * 10000000000000) / 10000000000000;
 		
 	};
-	
 
 }
 function insert(num){
-	textview.value = textview.value+ num
+	textview.value = textview.value+ num;
+	document.getElementById("divide").setAttribute("onclick", "divide()");
+	document.getElementById("multiply").setAttribute("onclick", "multiply()")
+	document.getElementById("plus").setAttribute("onclick", "plus()");
+	document.getElementById("minus").setAttribute("onclick", "minus()");
+	document.getElementById("point").setAttribute("onclick", "point()");
 };
+
+function divide(){
+	textview.value = textview.value+ "/";
+	document.getElementById("divide").removeAttribute("onclick", "divide()");
+
+};
+
+function minus(){
+	textview.value = textview.value+ "-";
+	document.getElementById("minus").removeAttribute("onclick", "minus()");
+};
+
+function plus(){
+	textview.value = textview.value+ "+";
+	document.getElementById("plus").removeAttribute("onclick", "plus()");
+};
+
+function multiply(){
+	textview.value = textview.value+ "*";
+	document.getElementById("multiply").removeAttribute("onclick", "multiply()");
+
+};
+
+function point(){
+	textview.value = textview.value+ ".";
+	document.getElementById("point").removeAttribute("onclick", "point()");
+}
