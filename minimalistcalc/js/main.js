6e5b24f54d9662d862dfe92781a9c9b3f3e6d36d
xmarkgs <mraner123@gmail.com>
2019-12-25 20:53:44 +0300

    Some bug fix

--- minimalistcalc/js/main.js
+++ minimalistcalc/js/main.js
@@ -3,19 +3,39 @@ var textview = document.getElementById('textview');
 
 function clean(){
 	textview.value = "";
+	document.getElementById("divide").setAttribute("onclick", "divide()");
+	document.getElementById("multiply").setAttribute("onclick", "multiply()")
+	document.getElementById("plus").setAttribute("onclick", "plus()");
+	document.getElementById("minus").setAttribute("onclick", "minus()");
+	document.getElementById("point").setAttribute("onclick", "point()");
+	document.getElementById("leftbracket").setAttribute("onclick", "leftbracket()");
+	document.getElementById("rightbracket").setAttribute("onclick", "rightbracket()");
 };
 function back(){
 	var value = textview.value;
 	textview.value = value.substring(0, value.length-1);
+	document.getElementById("divide").setAttribute("onclick", "divide()");
+	document.getElementById("multiply").setAttribute("onclick", "multiply()")
+	document.getElementById("plus").setAttribute("onclick", "plus()");
+	document.getElementById("minus").setAttribute("onclick", "minus()");
+	document.getElementById("point").setAttribute("onclick", "point()");
+	document.getElementById("leftbracket").setAttribute("onclick", "leftbracket()");
+	document.getElementById("rightbracket").setAttribute("onclick", "rightbracket()");
 };
 function equal(){
 	var valuenumber = textview.value;
 	if(valuenumber){
 		textview.value = Math.round(eval(valuenumber) * 10000000000000) / 10000000000000;
-		
 	};
-
+	// if (0){
+ // 		alert();
+	// } else if (error !== textview.value) {
+	// 	alert("Marko")
+	// }
 }
+var error; 
+// Має дорівнювати значенню інпута у всіх випадках крім неправильного
+
 function insert(num){
 	textview.value = textview.value+ num;
 	document.getElementById("divide").setAttribute("onclick", "divide()");
@@ -25,6 +45,7 @@ function insert(num){
 	document.getElementById("point").setAttribute("onclick", "point()");
 	document.getElementById("leftbracket").setAttribute("onclick", "leftbracket()");
 	document.getElementById("rightbracket").setAttribute("onclick", "rightbracket()");
+
 };
 
 function divide(){
@@ -76,12 +97,18 @@ function point(){
 	document.getElementById("divide").removeAttribute("onclick", "divide()");
 	document.getElementById("leftbracket").removeAttribute("onclick", "leftbracket()");
 	document.getElementById("rightbracket").removeAttribute("onclick", "rightbracket()");
+
 }
 
 function leftbracket(){
 	textview.value = textview.value+ "(";
+	document.getElementById("point").removeAttribute("onclick", "point()");
+	document.getElementById("multiply").removeAttribute("onclick", "multiply()");
+	document.getElementById("plus").removeAttribute("onclick", "plus()");
+	document.getElementById("minus").removeAttribute("onclick", "minus()");
+	document.getElementById("divide").removeAttribute("onclick", "divide()");
 }
 
 function rightbracket(){
 	textview.value = textview.value+ ")";
-}
+};
