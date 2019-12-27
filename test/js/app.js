90064db7b6d91282d4db34f99dbbd1d8e682537b
xmarkgs <mraner123@gmail.com>
2019-12-27 15:43:56 +0300

    Commit to Yura.S

--- test/js/app.js
+++ test/js/app.js
@@ -4,14 +4,14 @@ const username = document.querySelector("#username");
 const usernick = document.querySelector("#usernick");
 const userrepos = document.querySelector("#userrepositories");
 const userlink = document.querySelector("#userlink");
+const profile_pic = document.querySelector("#profile_pic");
+const usersite = document.querySelector("#usersite");
 
 const client_id = "66933062d61178e80d5a";
 const client_secret = "db3dcacd7470eedcc701438277930d25fdd053ec";
 
-// ?client_id=${client_id}&client_secret=${client_secret}
-
 const findUser = async (user) => {
-	const api_call = await fetch(`https://api.github.com/user/${user}?client_id=${client_id}&client_secret=${client_secret}`);
+	const api_call = await fetch(`https://api.github.com/users/${user}?client_id=${client_id}&client_secret=${client_secret}`);
 	const data = await api_call.json();
 	return {data}; 
 };
@@ -19,6 +19,14 @@ const findUser = async (user) => {
 const showData = () => {
 	findUser(inputValue.value).then((result) => {
 		console.log(result);
+		username.innerHTML = `Name: ${result.data.name}`;
+		usernick.innerHTML = `Nickname: ${result.data.login}`
+		userrepos.innerHTML = `The quantity of repositories: ${result.data.public_repos}`
+		userlink.innerHTML = `Link to his acc: ${result.data.html_url}`;
+		userlink.setAttribute("href", `${result.data.html_url}`);
+		profile_pic.setAttribute("src", `${result.data.avatar_url}`);
+		usersite.innerHTML = `His website: ${result.data.blog}`;
+		usersite.setAttribute("href", `${result.data.blog}`);
 	});
 };
 
