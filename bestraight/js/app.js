4e0cc9abac81707395a498217dcc3c68b4786f69
xmarkgs <mraner123@gmail.com>
2019-12-29 20:02:23 +0300

    Latest FIX MUST BE!

--- bestraight/js/app.js
+++ bestraight/js/app.js
@@ -1,17 +1,15 @@
 // JS App
 
 // Code Get Code
-// var window_code = window.location.search;
-// var code = window_code.slice(6, 35);
 var code = window.location.search;
 console.log(code);
 const xhr = new XMLHttpRequest();
-xhr.open("POST", `https://github.com/login/oauth/access_token${code}`);
-xhr.setRequestHeader('Accept', 'application/json');
+xhr.open("POST", "https://github.com/login/oauth/access_token");
+xhr.setRequestHeader("Accept", "application/json");
 xhr.send({
 	client_id: "83af441880ea9eca9533", 
 	client_secret: "90b880f712bae73b7a4433ce27501bfd7d86de35", 
-	code: `${code}`
+	code: code
 });
 xhr.onload = function (){
 	const access_token = this.responseText;
