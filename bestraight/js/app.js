390ec5a0084ceb062bca404df1716c1852319bda
xmarkgs <mraner123@gmail.com>
2019-12-29 14:40:50 +0300

    fix

--- bestraight/js/app.js
+++ bestraight/js/app.js
@@ -5,30 +5,28 @@ var window_code = window.location.search;
 var code = window_code.slice(6, 30);
 console.log(code);
 
-var xhr = new XMLHttpRequest();
-xhr.open('POST', 'https://github.com/login/oauth/access_token', true);
-xhr.setRequestHeader('Accept', 'application/json');
-xhr.send(JSON.stringify({
-    client_id: '83af441880ea9eca9533',
-    client_secret: '90b880f712bae73b7a4433ce27501bfd7d86de35'
-    code: `${code}`;
-}));
-xhr.then(function(res){
-	const data = res;
-	console.log(data);
+const getaccesstoken = new XMLHttpRequest();
+const urltoken='https://github.com/login/oauth/access_token';
+getaccesstoken.open("POST", urltoken);
+
+getaccesstoken.send({
+	client_id = '83af441880ea9eca9533',
+	client_secret = '90b880f712bae73b7a4433ce27501bfd7d86de35',
+	code = `${code}`
 });
-// (function() {
-//     `window.request = superagent;`
-//     superagent
-//       .post('https://github.com/login/oauth/access_token')
-//       .send({ 
-//       client_id: '83af441880ea9eca9533', 
-//       client_secret: '90b880f712bae73b7a4433ce27501bfd7d86de35',
-//       code: code 
-//       }) // sends a JSON post body
-//       .set('accept', 'json')
-//       .end(function (err, res) {
-//         const data = req.body;
-//         res.send(data);
-//       });
-//   })();
+
+getaccesstoken.onreadystatechange = (e) => {
+  console.log(getaccesstoken.responseText);
+};
+// var xhr = new XMLHttpRequest();
+// xhr.open('POST', 'https://github.com/login/oauth/access_token', true);
+// xhr.setRequestHeader('Accept', 'application/json');
+// xhr.send(JSON.stringify({
+//     client_id: '83af441880ea9eca9533',
+//     client_secret: '90b880f712bae73b7a4433ce27501bfd7d86de35'
+//     code: `${code}`;
+// }));
+// xhr.then(function(res){
+// 	const data = res;
+// 	console.log(data);
+// });
