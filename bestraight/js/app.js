14be54d9089a6ad2b908b6c0a7b73942e416e4b4
xmarkgs <mraner123@gmail.com>
2019-12-29 14:47:05 +0300

    fix

--- bestraight/js/app.js
+++ bestraight/js/app.js
@@ -9,11 +9,11 @@ const getaccesstoken = new XMLHttpRequest();
 const urltoken='https://github.com/login/oauth/access_token';
 getaccesstoken.open("POST", urltoken);
 
-getaccesstoken.send({
+getaccesstoken.send(JSON.stringify({
 	client_id = '83af441880ea9eca9533',
 	client_secret = '90b880f712bae73b7a4433ce27501bfd7d86de35',
 	code = `${code}`
-});
+}));
 
 getaccesstoken.onreadystatechange = (e) => {
   console.log(getaccesstoken.responseText);
