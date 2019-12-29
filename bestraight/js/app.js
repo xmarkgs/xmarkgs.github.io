0f6dc1ff102dec9aa71da9e508443d60798bdc7e
xmarkgs <mraner123@gmail.com>
2019-12-29 20:39:56 +0300

    fix

--- bestraight/js/app.js
+++ bestraight/js/app.js
@@ -6,11 +6,12 @@ var code = window_code.slice(6, 35);
 console.log(code);
 const xhr = new XMLHttpRequest();
 xhr.open("POST", "https://github.com/login/oauth/access_token");
+xhr.setRequestHeader("Access-Control-Allow-Origin");
 xhr.setRequestHeader("Accept", "application/json");
 xhr.send({
 	client_id: "83af441880ea9eca9533", 
 	client_secret: "90b880f712bae73b7a4433ce27501bfd7d86de35", 
-	code: code
+	code: `${code}`
 });
 xhr.onload = function (){
 	const access_token = this.responseText;
