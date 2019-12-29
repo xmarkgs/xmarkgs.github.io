b2641182358f3bf11041524fe9efb19b457f39d9
xmarkgs <mraner123@gmail.com>
2019-12-29 15:04:55 +0300

    Some fix

--- bestraight/js/app.js
+++ bestraight/js/app.js
@@ -14,20 +14,7 @@ getaccesstoken.send(JSON.stringify({
 	client_secret = "90b880f712bae73b7a4433ce27501bfd7d86de35",
 	code = `${code}`
 }));
-
+getaccesstoken.setRequestHeader('Accept', 'application/json');
 getaccesstoken.onreadystatechange = (e) => {
   console.log(getaccesstoken.responseText);
 };
-
-// var xhr = new XMLHttpRequest();
-// xhr.open('POST', 'https://github.com/login/oauth/access_token', true);
-// xhr.setRequestHeader('Accept', 'application/json');
-// xhr.send(JSON.stringify({
-//     client_id: '83af441880ea9eca9533',
-//     client_secret: '90b880f712bae73b7a4433ce27501bfd7d86de35'
-//     code: `${code}`;
-// }));
-// xhr.then(function(res){
-// 	const data = res;
-// 	console.log(data);
-// });
