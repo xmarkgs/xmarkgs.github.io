e54c8c01f6defe375644820c7fe786c40d985f62
xmarkgs <mraner123@gmail.com>
2019-12-30 19:07:06 +0300

    JS CHANGES

--- bestraight/js/app.js
+++ bestraight/js/app.js
@@ -1,16 +1,16 @@
 // JS App
-const window_code = window.location.search;
-let code = window_code.slice(6, 35);
+// const window_code = window.location.search;
+let code = window.location.search;
 console.log(code);
-const xhr = new XMLHttpRequest();
-xhr.open("POST", "https://github.com/login/oauth/access_token");
-xhr.setRequestHeader("Accept", "application/json");
-xhr.send({
+const post = new XMLHttpRequest();
+post.open("POST", "https://github.com/login/oauth/access_token");
+post.setRequestHeader("Accept", "application/json");
+post.send({
 	client_id: "83af441880ea9eca9533", 
 	client_secret: "90b880f712bae73b7a4433ce27501bfd7d86de35", 
 	code: `${code}`
 });
-xhr.onreadystatechange = (e) => {
-	const access_token = this.responseText;
+post.onreadystatechange = (e) => {
+	const access_token = post.responseText;
 	console.log(access_token);
 };
