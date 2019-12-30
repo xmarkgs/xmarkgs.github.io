5d282d88b9828f58778f319249663d0f9e792761
xmarkgs <mraner123@gmail.com>
2019-12-30 18:55:04 +0300

    onload fix

--- bestraight/js/app.js
+++ bestraight/js/app.js
@@ -4,13 +4,13 @@ let code = window_code.slice(6, 35);
 console.log(code);
 const xhr = new XMLHttpRequest();
 xhr.open("POST", "https://github.com/login/oauth/access_token");
-xhr.setRequestHeader("Accept", "text/html");
+xhr.setRequestHeader("Accept", "application/json");
 xhr.send({
 	client_id: "83af441880ea9eca9533", 
 	client_secret: "90b880f712bae73b7a4433ce27501bfd7d86de35", 
 	code: `${code}`
 });
-xhr.onload = function (){
+xhr.onreadystatechange = (e) => {
 	const access_token = this.responseText;
 	console.log(access_token);
 };
