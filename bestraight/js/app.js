30d4875216d80d2f23ceab116066477dbd951423
xmarkgs <mraner123@gmail.com>
2019-12-30 19:35:36 +0300

    fix

--- bestraight/js/app.js
+++ bestraight/js/app.js
@@ -4,7 +4,7 @@ let code = window.location.search;
 console.log(code);
 const post = new XMLHttpRequest();
 post.open("POST", "https://github.com/login/oauth/access_token");
-post.setRequestHeader("Accept", "application/json");
+post.setRequestHeader("Accept", "application/x-www-form-urlencoded");
 post.send({
 	client_id: "83af441880ea9eca9533", 
 	client_secret: "90b880f712bae73b7a4433ce27501bfd7d86de35", 
