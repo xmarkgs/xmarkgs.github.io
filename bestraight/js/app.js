d6c26b01dec0e85738b04c5191f6e9a07d628fe0
xmarkgs <mraner123@gmail.com>
2019-12-30 20:07:45 +0300

    fix detected

--- bestraight/js/app.js
+++ bestraight/js/app.js
@@ -1,18 +1,20 @@
 // JS App
 const window_code = window.location.search;
-let code = window_code.slice(6, 35);
+const code = window_code.slice(6, 35);
 console.log(code);
 const post = new XMLHttpRequest();
 const posturl = "https://github.com/login/oauth/access_token";
-post.open("POST", posturl);
-post.setRequestHeader("Access-Control-Allow-Origin", "https://github.com/login/oauth/access_token");
-post.setRequestHeader("Accept", "application/json");
-post.send({
+const exchangecode = {
 	client_id: "83af441880ea9eca9533", 
 	client_secret: "90b880f712bae73b7a4433ce27501bfd7d86de35", 
 	code: `${code}`
-});
+};
+post.open("POST", posturl);
+post.setRequestHeader("Access-Control-Allow-Origin", "https://github.com/login/oauth/access_token");
+post.setRequestHeader("Accept", "application/json");
+post.send(exchangecode);
 post.onreadystatechange = (e) => {
 	const access_token = post.responseText;
 	console.log(access_token);
 };
+// 
