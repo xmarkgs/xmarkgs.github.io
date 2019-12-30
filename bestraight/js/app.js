91d2049d4300eae450cc2096efd07a10f68fd30b
xmarkgs <mraner123@gmail.com>
2019-12-30 13:08:26 +0300

    Request Header fix

--- bestraight/js/app.js
+++ bestraight/js/app.js
@@ -1,11 +1,9 @@
 // JS App
-
-// Code Get Code
 var code = window.location.search;
 console.log(code);
 const xhr = new XMLHttpRequest();
 xhr.open("POST", "https://github.com/login/oauth/access_token");
-xhr.setRequestHeader("Accept", "application/json");
+xhr.setRequestHeader("Accept", "text/html");
 xhr.send({
 	client_id: "83af441880ea9eca9533", 
 	client_secret: "90b880f712bae73b7a4433ce27501bfd7d86de35", 
