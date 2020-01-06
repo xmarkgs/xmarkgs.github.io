8e7dbf21566f65381f8d9d67265658dabcecfab7
xmarkgs <mraner123@gmail.com>
2020-01-06 22:33:39 +0300

    another fix

--- bestraight/js/app.js
+++ bestraight/js/app.js
@@ -7,21 +7,22 @@ class GetUserToken extends React.Component{
 	constructor(props){
 		super(props);
 	}
-	const post = new XMLHttpRequest();
-	post.open("POST", "https://github.com/login/oauth/access_token");
-	post.setRequestHeader("Access-Control-Allow-Origin", "*");
-	post.setRequestHeader("Accept", "application/json");
-	post.send(JSON.stringify({
-		client_id: "83af441880ea9eca9533", 
-		client_secret: "90b880f712bae73b7a4433ce27501bfd7d86de35", 
-		code: `${code}`
-	}));
-	post.onreadystatechange = (e) => {
-		const access_token = post.responseText;
-		console.log(access_token);
-	};
 	render() {
-		<p>This is text.</p>
+		const post = new XMLHttpRequest();
+		post.open("POST", "https://github.com/login/oauth/access_token");
+		post.setRequestHeader("Access-Control-Allow-Origin", "*");
+		post.setRequestHeader("Accept", "application/json");
+		post.send(JSON.stringify({
+			client_id: "83af441880ea9eca9533", 
+			client_secret: "90b880f712bae73b7a4433ce27501bfd7d86de35", 
+			code: `${code}`
+		}));
+		post.onreadystatechange = (e) => {
+			const access_token = post.responseText;
+			console.log(access_token);
+		};
+		return	(<p>This is text.</p>);
+		
 	};
 };
 ReactDOM.render(
