499599febd6cf15f27457c4eeff6d44a1a1637da
xmarkgs <mraner123@gmail.com>
2020-01-06 16:55:45 +0300

    smol fix

--- bestraight/js/app.js
+++ bestraight/js/app.js
@@ -1,9 +1,9 @@
 // JS App
-const window_code = window.location.search;
-const code =  window_code.slice(6, 35);
+// const window_code = window.location.search;
+const code =  window.location.search;
 console.log(code);
 const post = new XMLHttpRequest();
-const posturl = "https://github.com/login/oauth/access_token";
+// const posturl = "https://github.com/login/oauth/access_token";
 const exchangecode = {
 	client_id: "83af441880ea9eca9533", 
 	client_secret: "90b880f712bae73b7a4433ce27501bfd7d86de35", 
