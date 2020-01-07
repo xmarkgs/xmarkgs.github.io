0bfe4903cb0f6fa2e7bb0558bcc10b07433ebfef
xmarkgs <mraner123@gmail.com>
2020-01-07 19:00:45 +0300

    POST method fix

--- bestraight/js/app.js
+++ bestraight/js/app.js
@@ -2,38 +2,28 @@
 'use strict';
 
 
+let window_code = window.location.search;
+const code = window_code.slice(6, 35);
+console.log(code + ' - is the user code');
 
-class GetUserToken extends React.Component{
-	constructor(props){
-		super(props);
-	};
-	componentWillMount() {
-    	this.getData();
-  	};
-  	getData(){
-  		let window_code = window.location.search;
-		const code =  window_code.slice(6, 35);
-		console.log(code);
-  		const post = new XMLHttpRequest();
-		post.open('POST', 'https://github.com/login/oauth/access_token', true);
-		post.setRequestHeader('Access-Control-Allow-Origin', '*');
-		post.setRequestHeader('Accept', 'application/json');
-		post.send(JSON.stringify({
-			client_id: '83af441880ea9eca9533', 
-			client_secret: '90b880f712bae73b7a4433ce27501bfd7d86de35', 
-			code: code
-		}));
-		post.addEventListener('load', () => {
-			const access_token = JSON.stringify(post.responseText);
-			console.log(access_token);
-		};
-		
-  	};
-  	render () {
-  		return (<p>This is yours: {access_token}</p>);
-  	};
+const post = new XMLHttpRequest();
+post.open('POST', 'https://github.com/login/oauth/access_token', true);
+post.setRequestHeader('Access-Control-Allow-Origin', '*');
+post.setRequestHeader('Accept', 'application/json');
+post.send(JSON.stringify({
+	client_id: '83af441880ea9eca9533', 
+	client_secret: '90b880f712bae73b7a4433ce27501bfd7d86de35', 
+	code: code
+}));
+post.addEventListener('load', () => {
+	const access_token = post.responseText;
+	console.log(access_token);
 };
-	
+
+function GetUserToken(){
+	return (<p>This is yours access_token: {access_token}</p>);
+}
+
 ReactDOM.render(
 	<GetUserToken />,
 	document.querySelector('.leftcol')
