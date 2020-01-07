a08d222053c1621898851fd9b78d227c1959a341
xmarkgs <mraner123@gmail.com>
2020-01-07 18:31:21 +0300

    JSON string fix

--- bestraight/js/app.js
+++ bestraight/js/app.js
@@ -1,9 +1,7 @@
 // JS App
 'use strict';
 
-let window_code = window.location.search;
-const code =  window_code.slice(6, 35);
-console.log(code);
+
 
 class GetUserToken extends React.Component{
 	constructor(props){
@@ -13,23 +11,27 @@ class GetUserToken extends React.Component{
     	this.getData();
   	};
   	getData(){
+  		let window_code = window.location.search;
+		const code =  window_code.slice(6, 35);
+		console.log(code);
   		const post = new XMLHttpRequest();
-		post.open("POST", "https://github.com/login/oauth/access_token");
-		post.setRequestHeader("Access-Control-Allow-Origin", "*");
-		post.setRequestHeader("Accept", "application/json");
+		post.open('POST', 'https://github.com/login/oauth/access_token', true);
+		post.setRequestHeader('Access-Control-Allow-Origin', '*');
+		post.setRequestHeader('Accept', 'application/json');
 		post.send(JSON.stringify({
-			client_id: "83af441880ea9eca9533", 
-			client_secret: "90b880f712bae73b7a4433ce27501bfd7d86de35", 
-			code: `${code}`
+			client_id: '83af441880ea9eca9533', 
+			client_secret: '90b880f712bae73b7a4433ce27501bfd7d86de35', 
+			code: code
 		}));
-		post.onreadystatechange = (e) => {
-			const access_token = post.responseText;
+		post.addEventListener('load', () => {
+			const access_token = JSON.stringify(post.responseText);
 			console.log(access_token);
 		};
+		
   	};
-  	render (){
-  		return (<p>This is a text.</p>);
-  	}
+  	render () {
+  		return (<p>This is yours: {access_token}</p>);
+  	};
 };
 	
 ReactDOM.render(
