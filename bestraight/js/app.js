907141841fef6dc22d57af471d56e4029149e196
xmarkgs <mraner123@gmail.com>
2020-01-07 21:40:29 +0300

    Trying to fix

--- bestraight/js/app.js
+++ bestraight/js/app.js
@@ -1,13 +1,13 @@
 // JS App
 'use strict';
 
-
+// Getting a user code
 let window_code = window.location.search;
 const code = window_code.slice(6, 35);
 console.log(code);
-
+// Sending a 'POST' request
 const post = new XMLHttpRequest();
-post.open('POST', 'https://github.com/login/oauth/access_token', true);
+post.open('POST', 'https://github.com/login/oauth/access_token');
 post.setRequestHeader('Access-Control-Allow-Origin', '*');
 post.setRequestHeader('Accept', 'application/json');
 post.send(JSON.stringify({
@@ -15,13 +15,13 @@ post.send(JSON.stringify({
 	client_secret: '90b880f712bae73b7a4433ce27501bfd7d86de35', 
 	code: code
 }));
-post.onreadystatechange = (e) => {
+post.onreadystatechange = () => {
 	const access_token = post.responseText;
 	console.log(access_token);
 };
 
 function GetUserToken(props){
-	return (<p>This is yours access_token: {access_token}</p>);
+	return (<p>This is yours access_token:</p>);
 }
 
 ReactDOM.render(
