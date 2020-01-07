99a1262e5d9eb4a7b09d7104f313ea510d741b16
xmarkgs <mraner123@gmail.com>
2020-01-07 17:48:02 +0300

    fix

--- bestraight/js/app.js
+++ bestraight/js/app.js
@@ -1,24 +1,37 @@
 // JS App
 'use strict';
+
 let window_code = window.location.search;
 const code =  window_code.slice(6, 35);
 console.log(code);
-const post = new XMLHttpRequest();
-post.open("POST", "https://github.com/login/oauth/access_token");
-post.setRequestHeader("Access-Control-Allow-Origin", "*");
-post.setRequestHeader("Accept", "application/json");
-post.send(JSON.stringify({
-	client_id: "83af441880ea9eca9533", 
-	client_secret: "90b880f712bae73b7a4433ce27501bfd7d86de35", 
-	code: `${code}`
-}));
-post.onreadystatechange = (e) => {
-	const access_token = post.responseText;
-	console.log(access_token);
+
+class GetUserToken extends React.Component{
+	constructor(props){
+		super(props);
+	};
+	componentWillMount() {
+    	this.getData();
+  	};
+  	getData(){
+  		const post = new XMLHttpRequest();
+		post.open("POST", "https://github.com/login/oauth/access_token");
+		post.setRequestHeader("Access-Control-Allow-Origin", "*");
+		post.setRequestHeader("Accept", "application/json");
+		post.send(JSON.stringify({
+			client_id: "83af441880ea9eca9533", 
+			client_secret: "90b880f712bae73b7a4433ce27501bfd7d86de35", 
+			code: `${code}`
+		}));
+		post.onreadystatechange = (e) => {
+			const access_token = post.responseText;
+			console.log(access_token);
+		};
+  	};
+  	render (){
+  		return (<p>This is a text.</p>);
+  	}
 };
-function GetUserToken(props){
-	return(<p>This is a text. </p>);
-};
+	
 ReactDOM.render(
 	<GetUserToken />,
 	document.querySelector('.leftcol')
