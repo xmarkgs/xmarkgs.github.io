08f52cccf273d88406c5a4e5dfff7c3ccf492a2d
xmarkgs <mraner123@gmail.com>
2020-02-05 23:17:52 +0300

    Optimized ver.
    Deleted comments.

--- blindtyping/js/main.js
+++ blindtyping/js/main.js
@@ -47,16 +47,6 @@ function caseShorter() {
 	}, 2000);
 }
 
-// function caseShorter1() {
-// 	userinput.setAttribute("readonly", "");
-// 	setTimeout(() => {
-// 		document.getElementById("game_section").style.background = "linear-gradient(23.39deg, #5B1D82, #7D00CA)";
-// 		userinput.removeAttribute("readonly");
-// 		randomNumbers();
-// 		userinput.value = "";
-// 	}, 2000);
-// }
-
 userinput.oninput = () => {
 	gameFunction();
 };
@@ -65,204 +55,31 @@ function gameFunction() {
 	let isGreen;
 	if (randomizer == 0) {
 		isGreen = false;
-		letters[rand-1].forEach(function(item){
-			if (userinput.value == item){isGreen = true;}
+		letters[rand - 1].forEach(function (item) {
+			if (userinput.value == item) {
+				isGreen = true;
+			}
 		});
-		
-		if (isGreen == true){
+
+		if (isGreen == true) {
 			document.getElementById("game_section").style.background = "linear-gradient(23.39deg, #269e4e, rgb(20, 199, 79))";
-		} else if(isGreen == false){
+		} else if (isGreen == false) {
 			document.getElementById("game_section").style.background = "linear-gradient(23.39deg, rgb(194, 40, 40), rgb(240, 61, 61))";
 		}
 		caseShorter();
-		/*
-		switch (rand) {
-			case 1:
-				if (userinput.value == "q" || userinput.value == "a" || userinput.value == "z" || userinput.value == "Q" || userinput.value == "A" || userinput.value == "Z" || userinput.value == "1") {
-					document.getElementById("game_section").style.background = "linear-gradient(23.39deg, #269e4e, rgb(20, 199, 79))";
-				} else {
-					document.getElementById("game_section").style.background = "linear-gradient(23.39deg, rgb(194, 40, 40), rgb(240, 61, 61))";
-				}
-				caseShorter1();
-				break;
-			case 2:
-				if (userinput.value == "w" || userinput.value == "s" || userinput.value == "x" || userinput.value == "W" || userinput.value == "S" || userinput.value == "X" || userinput.value == "2") {
-					document.getElementById("game_section").style.background = "linear-gradient(23.39deg, #269e4e, rgb(20, 199, 79))";
-				} else {
-					document.getElementById("game_section").style.background = "linear-gradient(23.39deg, rgb(194, 40, 40), rgb(240, 61, 61))";
-				}
-				caseShorter1();
-				break;
-			case 3:
-				if (userinput.value == "e" || userinput.value == "d" || userinput.value == "c" || userinput.value == "E" || userinput.value == "D" || userinput.value == "C" || userinput.value == "3") {
-					document.getElementById("game_section").style.background = "linear-gradient(23.39deg, #269e4e, rgb(20, 199, 79))";
-				} else {
-					document.getElementById("game_section").style.background = "linear-gradient(23.39deg, rgb(194, 40, 40), rgb(240, 61, 61))";
-				}
-				caseShorter1();
-				break;
-			case 4:
-				if (userinput.value == "r" || userinput.value == "f" || userinput.value == "v" || userinput.value == "R" || userinput.value == "F" || userinput.value == "V" || userinput.value == "4" || userinput.value == "5" || userinput.value == "t" || userinput.value == "T" || userinput.value == "g" || userinput.value == "G" || userinput.value == "b" || userinput.value == "B") {
-					document.getElementById("game_section").style.background = "linear-gradient(23.39deg, #269e4e, rgb(20, 199, 79))";
-				} else {
-					document.getElementById("game_section").style.background = "linear-gradient(23.39deg, rgb(194, 40, 40), rgb(240, 61, 61))";
-				}
-				caseShorter1();
-				break;
-			case 5:
-				if (userinput.value == " ") {
-					document.getElementById("game_section").style.background = "linear-gradient(23.39deg, #269e4e, rgb(20, 199, 79))";
-				} else {
-					document.getElementById("game_section").style.background = "linear-gradient(23.39deg, rgb(194, 40, 40), rgb(240, 61, 61))";
-				}
-				caseShorter1();
-				break;
-			case 8:
-				if (userinput.value == "i" || userinput.value == "k" || userinput.value == "," || userinput.value == "I" || userinput.value == "K" || userinput.value == "8") {
-					document.getElementById("game_section").style.background = "linear-gradient(23.39deg, #269e4e, rgb(20, 199, 79))";
-				} else {
-					document.getElementById("game_section").style.background = "linear-gradient(23.39deg, rgb(194, 40, 40), rgb(240, 61, 61))";
-				}
-				caseShorter1();
-				break;
-			case 7:
-				if (userinput.value == "y" || userinput.value == "h" || userinput.value == "n" || userinput.value == "Y" || userinput.value == "H" || userinput.value == "N" || userinput.value == "6" || userinput.value == "7" || userinput.value == "u" || userinput.value == "U" || userinput.value == "j" || userinput.value == "J" || userinput.value == "m" || userinput.value == "M") {
-					document.getElementById("game_section").style.background = "linear-gradient(23.39deg, #269e4e, rgb(20, 199, 79))";
-				} else {
-					document.getElementById("game_section").style.background = "linear-gradient(23.39deg, rgb(194, 40, 40), rgb(240, 61, 61))";
-				}
-				caseShorter1();
-				break;
-			case 9:
-				if (userinput.value == "o" || userinput.value == "l" || userinput.value == "." || userinput.value == "O" || userinput.value == "L" || userinput.value == "9") {
-					document.getElementById("game_section").style.background = "linear-gradient(23.39deg, #269e4e, rgb(20, 199, 79))";
-				} else {
-					document.getElementById("game_section").style.background = "linear-gradient(23.39deg, rgb(194, 40, 40), rgb(240, 61, 61))";
-				}
-				caseShorter1();
-				break;
-			case 6:
-				if (userinput.value == " ") {
-					document.getElementById("game_section").style.background = "linear-gradient(23.39deg, #269e4e, rgb(20, 199, 79))";
-				} else {
-					document.getElementById("game_section").style.background = "linear-gradient(23.39deg, rgb(194, 40, 40), rgb(240, 61, 61))";
-				}
-				caseShorter1();
-			case 10:
-				if (userinput.value == "0" || userinput.value == "p" || userinput.value == "P" || userinput.value == "[" || userinput.value == "]" || userinput.value == "}" || userinput.value == "{" || userinput.value == "-" || userinput.value == "'" || userinput.value == "|" || userinput.value == ";" || userinput.value == ":" || userinput.value == "/" || userinput.value == "?") {
-					document.getElementById("game_section").style.background = "linear-gradient(23.39deg, #269e4e, rgb(20, 199, 79))";
-				} else {
-					document.getElementById("game_section").style.background = "linear-gradient(23.39deg, rgb(194, 40, 40), rgb(240, 61, 61))";
-				}
-				caseShorter1();
-				break;
-		}
-	*/
 	} else if (randomizer == 1) {
 		isGreen = false;
-		letters[userinput.value-1].forEach(function(item){
-			if (randomLetterValue == item){isGreen = true;}
-		});
-		
-		if (isGreen == true){
-			document.getElementById("game_section").style.background = "linear-gradient(23.39deg, #269e4e, rgb(20, 199, 79))";
-		} else if(isGreen == false){
-			document.getElementById("game_section").style.background = "linear-gradient(23.39deg, rgb(194, 40, 40), rgb(240, 61, 61))";
+			letters[userinput.value - 1].forEach(function (item) {
+				if (randomLetterValue == item) {
+					isGreen = true;
+				}
+			});
+
+			if (isGreen == true) {
+				document.getElementById("game_section").style.background = "linear-gradient(23.39deg, #269e4e, rgb(20, 199, 79))";
+			} else if (isGreen == false) {
+				document.getElementById("game_section").style.background = "linear-gradient(23.39deg, rgb(194, 40, 40), rgb(240, 61, 61))";
+			}
+			caseShorter();
 		}
-		caseShorter();
-	// 	switch (randomLetterValue) {
-	// 		case "Q":
-	// 		case "A":
-	// 		case "Z":
-	// 			if (userinput.value == "1") {
-	// 				document.getElementById("game_section").style.background = "linear-gradient(23.39deg, #269e4e, rgb(20, 199, 79))";
-	// 			} else {
-	// 				document.getElementById("game_section").style.background = "linear-gradient(23.39deg, rgb(194, 40, 40), rgb(240, 61, 61))";
-	// 			}
-	// 			caseShorter2();
-	// 			break;
-	// 		case "W":
-	// 		case "S":
-	// 		case "X":
-	// 			if (userinput.value == "2") {
-	// 				document.getElementById("game_section").style.background = "linear-gradient(23.39deg, #269e4e, rgb(20, 199, 79))";
-	// 			} else {
-	// 				document.getElementById("game_section").style.background = "linear-gradient(23.39deg, rgb(194, 40, 40), rgb(240, 61, 61))";
-	// 			}
-	// 			caseShorter2();
-	// 			break;
-	// 		case "E":
-	// 		case "D":
-	// 		case "C":
-	// 			if (userinput.value == "3") {
-	// 				document.getElementById("game_section").style.background = "linear-gradient(23.39deg, #269e4e, rgb(20, 199, 79))";
-	// 			} else {
-	// 				document.getElementById("game_section").style.background = "linear-gradient(23.39deg, rgb(194, 40, 40), rgb(240, 61, 61))";
-	// 			}
-	// 			caseShorter2();
-	// 			break;
-	// 		case "R":
-	// 		case "F":
-	// 		case "V":
-	// 		case "T":
-	// 		case "G":
-	// 		case "B":
-	// 			if (userinput.value == "4") {
-	// 				document.getElementById("game_section").style.background = "linear-gradient(23.39deg, #269e4e, rgb(20, 199, 79))";
-	// 			} else {
-	// 				document.getElementById("game_section").style.background = "linear-gradient(23.39deg, rgb(194, 40, 40), rgb(240, 61, 61))";
-	// 			}
-	// 			caseShorter2();
-	// 			break;
-	// 		case " ":
-	// 			if (userinput.value == "5" || userinput.value == "6") {
-	// 				document.getElementById("game_section").style.background = "linear-gradient(23.39deg, #269e4e, rgb(20, 199, 79))";
-	// 			} else {
-	// 				document.getElementById("game_section").style.background = "linear-gradient(23.39deg, rgb(194, 40, 40), rgb(240, 61, 61))";
-	// 			}
-	// 			caseShorter2();
-	// 			break;
-	// 		case "Y":
-	// 		case "H":
-	// 		case "N":
-	// 		case "U":
-	// 		case "J":
-	// 		case "M":
-	// 			if (userinput.value == "7") {
-	// 				document.getElementById("game_section").style.background = "linear-gradient(23.39deg, #269e4e, rgb(20, 199, 79))";
-	// 			} else {
-	// 				document.getElementById("game_section").style.background = "linear-gradient(23.39deg, rgb(194, 40, 40), rgb(240, 61, 61))";
-	// 			}
-	// 			caseShorter2();
-	// 			break;
-	// 		case "I":
-	// 		case "K":
-	// 			if (userinput.value == "8") {
-	// 				document.getElementById("game_section").style.background = "linear-gradient(23.39deg, #269e4e, rgb(20, 199, 79))";
-	// 			} else {
-	// 				document.getElementById("game_section").style.background = "linear-gradient(23.39deg, rgb(194, 40, 40), rgb(240, 61, 61))";
-	// 			}
-	// 			caseShorter2();
-	// 			break;
-	// 		case "O":
-	// 		case "L":
-	// 			if (userinput.value == "9") {
-	// 				document.getElementById("game_section").style.background = "linear-gradient(23.39deg, #269e4e, rgb(20, 199, 79))";
-	// 			} else {
-	// 				document.getElementById("game_section").style.background = "linear-gradient(23.39deg, rgb(194, 40, 40), rgb(240, 61, 61))";
-	// 			}
-	// 			caseShorter2();
-	// 			break;
-	// 		case "P":
-	// 			if (userinput.value == "10") {
-	// 				document.getElementById("game_section").style.background = "linear-gradient(23.39deg, #269e4e, rgb(20, 199, 79))";
-	// 			} else {
-	// 				document.getElementById("game_section").style.background = "linear-gradient(23.39deg, rgb(194, 40, 40), rgb(240, 61, 61))";
-	// 			}
-	// 			caseShorter2();
-	// 			break;
-	// 	}
-	// }
-	// randomizer = Math.round(Math.random());
-}
 }
