3d582b3f11227430145d2b0bd3aa248dec08eaf3
xmarkgs <mraner123@gmail.com>
2020-04-24 13:40:08 +0300

    added legtah website

---
+++ legtah/js/main.js
@@ -0,0 +1,50 @@
+window.addEventListener("DOMContentLoaded", function () {
+    let slideIndex = 1,
+        slides = document.querySelectorAll(".slider-item"),
+        prev = document.querySelector(".prev"),
+        next = document.querySelector(".next"),
+        dotsWrap = document.querySelector(".slider-dots"),
+        dots = document.querySelectorAll(".dot");
+
+    showSlides(slideIndex);
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => item.style.display = "none");
+        dots.forEach((item) => item.classList.remove("dot-active"));
+
+        slides[slideIndex - 1].style.display = "grid";
+        dots[slideIndex - 1].classList.add("dot-active");
+
+    }
+
+    function plusSlide(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener("click", function () {
+        plusSlide(-1);
+    });
+
+    next.addEventListener("click", function () {
+        plusSlide(1);
+    });
+
+    dotsWrap.addEventListener("click", function (event) {
+        for (let i = 0; i < dots.length + 1; i++) {
+            if (event.target.classList.contains("dot") && event.target == dots[i - 1]) {
+                currentSlide(i);
+            }
+        }
+    });
+});
